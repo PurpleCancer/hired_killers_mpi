5172e14464f5a2ba5ebc6e3939a04c0bf1acf5da
Jan Bajer <jan.bajer@gmail.com>
2017-06-09 12:43:32 +0200

    Simple MPI program

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char ** argv)
 {
+    int size, rank;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     cout<<"Hello world!"<<endl;
+
+    MPI_Finalize();
     
     return 0;
 }
