b178a1795a78e3cc49583f2b4ee4dfb7a0b233c2
Jan Bajer <jan.bajer@gmail.com>
2017-06-11 00:55:50 +0200

    Company initialization added.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <mpi.h>
+#include <ctime>
+#include <vector>
 #include "CompanyRequest.hpp"
 #include "Company.hpp"
+#include "Definitions.hpp"
 
 using namespace std;
 
@@ -10,12 +14,46 @@ int main(int argc, char ** argv)
 {
     int size, rank;
 
+    int numberOfCompanies;
+    vector<Company *> companies;
+
     MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    cout<<"Hello world!"<<endl;
+    // Initialization
+    {    
+        int randomSeed;
+        // Randomize initial values
+        if (rank == ROOT)
+        {
+            srand(time(0));
+            if (NUMBER_OF_COMPANIES == 0)
+                numberOfCompanies = rand()%(MAX_NUMBER_OF_COMPANIES - 1) + 1;
+            else
+                numberOfCompanies = NUMBER_OF_COMPANIES;
+
+            randomSeed = rand();
+        }
+
+        MPI_Bcast(&numberOfCompanies, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+        MPI_Bcast(&randomSeed, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+
+        srand(randomSeed);
+
+        for (int i = 0; i < numberOfCompanies; ++i)
+        {
+            int killers = rand()%(MAX_NUMBER_OF_KILLERS - MIN_NUMBER_OF_KILLERS + 1) + MIN_NUMBER_OF_KILLERS;
+            int seed = rand();
+            companies.push_back(new Company(killers, seed));
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    
+    //Algorithm
+    
 
     MPI_Finalize();
     
